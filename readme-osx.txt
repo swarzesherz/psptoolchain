42db62d039007f889f04fe547aa518b5d4c17431
Herz <achwazer@gmail.com>
2011-08-01 12:13:00 -0500

    Use macports to get libusb and build psplinkusb

--- readme-osx.txt
+++ readme-osx.txt
@@ -4,7 +4,7 @@ Go to http://www.macports.org/install.php and follow the instructions
 ## Install the required packages.
 sudo port install autoconf autogen automake coreutils expat bison flex gmp \ 
 gperf guile libiconv libidn libtool libxml2 lzmautils m4 mpfr ncurses ncursesw \
-p5-locale-gettext pkgconfig readline texinfo wget zlib perl5 gcc44 libsdl
+p5-locale-gettext pkgconfig readline texinfo wget zlib perl5 gcc44 libsdl libusb-compat
 
 ## Build and install the toolchain + sdk. A login script will automatically 
 ## be created in /etc/launchd.conf if possible.
--- scripts/009-psplinkusb.sh
+++ scripts/009-psplinkusb.sh
@@ -4,17 +4,6 @@
  ## Exit on errors
  set -e
 
- ## Darwin build extras
- if [ `uname -s | grep -o "Darwin"` ]; then
-	export CC=gcc-4.2
-	wget --continue --no-check-certificate http://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-1.0.8/libusb-1.0.8.tar.bz2/download -O libusb.tar.gz
- 	rm -Rf libusb && mkdir libusb && tar --strip-components=1 --directory=libusb -xvzf libusb.tar.gz
-	cd libusb
-	./configure
-	make
-	make install
- fi
-
  ## Download the source code.
  wget --continue --no-check-certificate https://github.com/pspdev/psplinkusb/tarball/master -O psplinkusb.tar.gz
 
